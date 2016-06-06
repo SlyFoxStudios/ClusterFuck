ef9db9253dff40456e57c53bd2e80a3ae9eb0702
Daniel Jones <xXDJONESXx@hotmail.com>
2016-06-07 08:15:38 +1000

    Added player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
+	public float speed = 10f;
+
 	void Start () {
-	
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+
+		float moveSpeed = speed * Time.deltaTime;
+
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += new Vector3 (0, moveSpeed);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position += new Vector3 (-moveSpeed, 0);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position += new Vector3 (0, -moveSpeed);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position += new Vector3 (moveSpeed, 0);
+		}
+	}
+
+	void Shoot()
+	{
 	}
 }
