b45c1e998427c4ea9c4253224ff1aaa527b0ffd7
crazywolf132 <crazywolf132@gmail.com>
2016-06-07 07:52:35 +1000

    Finished player and bullets

--- Assets/Scripts/PlayerScript.js
+++ Assets/Scripts/PlayerScript.js
@@ -1,9 +1,21 @@
-﻿#pragma strict
+﻿// A variable that will contain our bullet prefab
+public var bullet : GameObject;
 
-function Start () {
+function Update() { 
+    // Move the spaceship horizontally (no changes)
+    var r2d = GetComponent("Rigidbody2D");
+    if (Input.GetKey("right"))
+        r2d.velocity.x = 10;
+    else if (Input.GetKey("left"))
+        r2d.velocity.x = -10;
+    else
+        r2d.velocity.x = 0;
 
-}
-
-function Update () {
-
-}
+    // When the spacebar is pressed
+    if (Input.GetKeyDown("space")) {
+        // Create a new bullet at “transform.position” 
+        // Which is the current position of the ship
+        // Quaternion.identity = add the bullet with no rotation
+        Instantiate(bullet, transform.position, Quaternion.identity);
+    }
+} 
