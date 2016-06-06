bd3045edcbc91600042e48a9041d99ffa8f97498
Daniel Jones <xXDJONESXx@hotmail.com>
2016-06-07 08:24:41 +1000

    Added player rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,18 +12,30 @@ public class Player : MonoBehaviour {
 
 		float moveSpeed = speed * Time.deltaTime;
 
+
+		//WASD Keys
 		if (Input.GetKey (KeyCode.W)) {
 			transform.position += new Vector3 (0, moveSpeed);
 		}
+		else
 		if (Input.GetKey (KeyCode.A)) {
 			transform.position += new Vector3 (-moveSpeed, 0);
 		}
+		else
 		if (Input.GetKey (KeyCode.S)) {
 			transform.position += new Vector3 (0, -moveSpeed);
 		}
+		else
 		if (Input.GetKey (KeyCode.D)) {
 			transform.position += new Vector3 (moveSpeed, 0);
 		}
+
+		//Look at mouse
+		Vector3 mouse = Input.mousePosition;
+		Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+		Vector2 offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+		float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(0, 0, angle);
 	}
 
 	void Shoot()
