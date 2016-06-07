80a266f45b3fcfaa848571b115a988c22eea84cb
Daniel Jones <xXDJONESXx@hotmail.com>
2016-06-08 06:42:00 +1000

    Meta progress

---
+++ Assets/Scripts/Lazer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lazer : MonoBehaviour {
+
+	float moveSpeed = 20f;
+
+	Rigidbody2D rigidBody;
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	void Update () {
+	}
+
+	void OnBecameInvisible()
+	{
+		Destroy (gameObject);
+	}
+}
